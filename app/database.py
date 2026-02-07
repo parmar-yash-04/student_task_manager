2006d7a4eaeb8c37b2bb18309db91b2bfcc7ced2
User <user@example.com>
2026-02-07 13:01:49 +0530

    Updated code for deploy

--- app/database.py
+++ app/database.py
@@ -2,8 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={"sslmode": "require"} if "render.com" in settings.database_url else {}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
