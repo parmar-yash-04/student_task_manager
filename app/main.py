e9c9ef774fe0f25a251772aa1cbaa80d5bed9544
User <user@example.com>
2026-02-07 10:53:49 +0530

    Done

--- app/main.py
+++ app/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI, Depends
 from app.models import User
 from app.database import engine, Base
 from app.routers import user, task, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main():
